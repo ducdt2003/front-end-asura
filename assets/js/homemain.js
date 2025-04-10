fde1505f103fab239c165ce508d843fc9bc87002
ducdt2003 <dinhtrongduc2003@gmail.com>
2025-04-10 16:29:19 +0700

    update giao dien home

--- assets/js/homemain.js
+++ assets/js/homemain.js
@@ -1,4 +1,16 @@
 
+// js chuyển động đầu
+document.addEventListener('DOMContentLoaded', () => {
+    const carousel = document.querySelector('#comicCarousel');
+    const dots = document.querySelectorAll('.scroll-dot');
+  
+    carousel.addEventListener('slide.bs.carousel', function (e) {
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[e.to].classList.add('active');
+    });
+  });
+
+  
 // js truyện nỗi tiếng 
 
 document.querySelectorAll(".scroll-dot").forEach((dot, index) => {
@@ -13,3 +25,7 @@ document.querySelectorAll(".scroll-dot").forEach((dot, index) => {
         dot.classList.add("active");
     });
 });
+
+
+
+
